d048e8172cafb9025bf977f6c481a5d3003ad59e
Chaitanya Naphade <chaitanya.naphade@gmail.com>
2023-12-02 16:30:58 -0800

    updates to fix UI, check previous commit for OG submit

--- src/components/Clause.js
+++ src/components/Clause.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
-const Clause = ({ children }) => {
+// Global counter and node coloring for clause numbering
+let counter = {};
+let visited = [];
+
+const Clause = ({ item, level, parseJsonData, style }) => {
+  // Initialize counter for the current level if not already set
+  if (counter[level] === undefined) {
+    counter[level] = 0;
+  }
+
+  // Reset counter for all levels greater than current
+  for (let i = level + 1; counter.hasOwnProperty(i); i++) {
+    counter[i] = 0;
+  }
+
+  // Increment counter for the current level if node not visited
+  if (!visited.includes(item)) {
+    counter[level]++;
+    visited.push(item);
+  }
+
+  // Alternate between lettering and numbering
+  const listItemNumber = level % 2 === 0 ? `${counter[level]}.` : `(${String.fromCharCode(97 + counter[level] - 1)})`;
+
   return (
-    <div className='clause'>
-      {children}
+    <div className='clause' style={{...style, display: 'inline-block'}}>
+      {listItemNumber} {item.children ? parseJsonData(item.children, level + 1) : item.text}
     </div>
   );
 };
--- src/parseJsonData.js
+++ src/parseJsonData.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import Clause from './components/Clause'
 
-
-const parseJsonData = (data) => {
+const parseJsonData = (data, level = 0) => {
 	if (!Array.isArray(data)) {
 		return null;
 	}
@@ -29,34 +29,34 @@ const parseJsonData = (data) => {
 		}
 
 		switch (item.type) {
-			case 'block':
-				return <div key={index}>{parseJsonData(item.children)}</div>;
-			case 'h1':
-				return <h1 key={index} style={style}>{parseJsonData(item.children)}</h1>;
-			case 'p':
-				return item.children ?
-					<div key={index} className='paragraph'>{parseJsonData(item.children)}</div> :
-					<p key={index} style={style}>{item.text}</p>;
+			case 'clause':
+				return <Clause key={index} item={item} level={level} parseJsonData={parseJsonData} style={style} />;
 			case 'mention':
 				return (
 					<span key={index} className='mention' style={{ backgroundColor: item.color }}>
-						{parseJsonData(item.children)}
+						{parseJsonData(item.children, level)}
 					</span>
 				);
-			case 'ul':
-				return <ul key={index}>{parseJsonData(item.children)}</ul>;
-			case 'li':
-				return <li key={index} style={style}>{parseJsonData(item.children)}</li>;
-			case 'lic':
-				return <div key={index} className='custom-list-item'>{parseJsonData(item.children)}</div>;
+			case 'block':
+				return <div key={index}>{parseJsonData(item.children, level)}</div>;
+			case 'h1':
+				return <h1 key={index} style={style}>{parseJsonData(item.children, level)}</h1>;
 			case 'h4':
-				return <h4 key={index} style={style}>{parseJsonData(item.children)}</h4>;
-			case 'clause':
-				return <div key={index} className='clause'>{parseJsonData(item.children)}</div>;
+				return <h4 key={index} style={{ ...style, display: 'inline' }}>{parseJsonData(item.children, level)}</h4>;
+			case 'ul':
+				return <ul key={index} style={style}>{parseJsonData(item.children, level)}</ul>;
+			case 'li':
+				return <li key={index} style={style}>{parseJsonData(item.children, level)}</li>;
+			case 'lic':
+				return <div key={index} style={style}>{parseJsonData(item.children, level)}</div>;
+			case 'p':
+				return item.children ?
+					<div key={index} style={{ ...style, display: 'inline' }}>{parseJsonData(item.children, level)}</div> :
+					<p key={index} style={{ ...style, display: 'inline' }}>{item.text}</p>;
 			default:
 				return (
 					<span key={index}>
-						{item.text ? renderTextWithLineBreaks(item.text, style) : parseJsonData(item.children)}
+						{item.text ? renderTextWithLineBreaks(item.text, style) : parseJsonData(item.children, level)}
 					</span>
 				);
 		}
